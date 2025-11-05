23b8387ab5e1a07fef205aeccbddf1749afe2a1d
thizplus <info@thizplus.com>
2025-11-05 11:05:11 +0700

    add text

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -18,10 +18,10 @@ export default function RegisterPage() {
 
             <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">
-                    {process.env.NEXT_PUBLIC_APP_NAME || "My App"}
+                    {process.env.NEXT_PUBLIC_APP_NAME }
                   </span>
                   <span className="truncate text-xs">
-                    {process.env.NEXT_PUBLIC_APP_PLAN || "Free Plan"}
+                    {process.env.NEXT_PUBLIC_APP_PLAN}
                   </span>
                 </div>
         </a>
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -107,10 +107,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">
-                    {process.env.NEXT_PUBLIC_APP_NAME || "My App"}
+                    {process.env.NEXT_PUBLIC_APP_NAME}
                   </span>
                   <span className="truncate text-xs">
-                    {process.env.NEXT_PUBLIC_APP_PLAN || "Free Plan"}
+                    {process.env.NEXT_PUBLIC_APP_PLAN}
                   </span>
                 </div>
               </a>
