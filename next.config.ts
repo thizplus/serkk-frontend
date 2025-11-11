d102044a173a25dc6d2dc45e9a7d43566e111522
thizplus <info@thizplus.com>
2025-11-12 02:00:21 +0700

    edit nextjs config

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
+
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
@@ -19,23 +14,10 @@ const nextConfig: NextConfig = {
       // Cloudflare R2 (New)
       {
         protocol: 'https',
-        hostname: 'pub-a058b390b77f486aaf97a1d1f073c6c8.r2.dev',
+        hostname: 'pub-8bc881ced04b42bab801e0bed862958c.r2.dev',
         port: '',
         pathname: '/**',
-      },
-      // Bunny CDN (Legacy - for old images)
-      {
-        protocol: 'https',
-        hostname: 'voobizethailand.b-cdn.net',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'vz-b1631ae0-4c8.b-cdn.net',
-        port: '',
-        pathname: '/**',
-      },
+      }
     ],
   },
 };
